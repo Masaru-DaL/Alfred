dd202621372f390909f38e4bf7e6a7f36634a238
Masaru <hittec9609@gmail.com>
2023-11-17 17:40:03 +0900

    executed by Automator.

--- change.sh
+++ change.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-# 変更を加えたいディレクトリへのパスを指定
-target_directory="/Users/masaru/Alfred"
+# 移動元ディレクトリのパスを指定
+source_directory="/Users/masaru/Alfred"
 
-# 指定したディレクトリ内の全ての.csvファイルを再帰的に取得
-find "$target_directory" -type f -name "*.csv" -print0 | while IFS= read -r -d $'\0' csv_file; do
-    # ファイル名を取得
-    file_name=$(basename "$csv_file")
+# 移動先ディレクトリのパスを指定
+destination_directory="/Users/masaru/Library/Application Support/Alfred/Alfred.alfredpreferences/workflows"
 
-    # 拡張子を取り除いたファイル名を取得
-    file_name_without_extension="${file_name%.*}"
+# 移動元ディレクトリ内の全ての.csvファイルを再帰的に取得し、移動先ディレクトリに移動
+find "$source_directory" -type f -name "*.csv" -exec mv {} "$destination_directory" \;
 
-    # 新しいファイル名を生成
-    new_file_name="${file_name_without_extension}_list.csv"
-
-    # ファイル名を変更
-    mv "$csv_file" "$(dirname "$csv_file")/$new_file_name"
-
-    echo "変更前のファイル名: $file_name"
-    echo "変更後のファイル名: $new_file_name"
-done
-
-echo "全ての.csvファイルの名前を変更しました。"
+echo ".csvファイルを移動しました。"
